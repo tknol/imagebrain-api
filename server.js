4e5c7cd351424c7d77c3111707cc77dd55b15980
Thomas <tom.n.knol@gmail.com>
2018-08-06 09:16:28 -0500

    added methods to handle signin and register

--- server.js
+++ server.js
@@ -2,11 +2,61 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users:[
+    {
+      id: '123',
+      name: 'john',
+      email: 'meatman@mail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'sally',
+      email: 'sizzle@mail.com',
+      password: 'banana',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
-  res.send("yep");
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  if(req.body.email == database.users[0].email && req.body.password == database.users[0].password) {
+      res.json('success');
+  } else{
+    res.status(400).json('error logging in')
+  }
+
+})
+
+app.post('/register', (req, res) => {
+  const {email, name, password} = req.body;
+  database.users.push({
+      id: '125',
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+  })
+  res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
+
+// / root -- get --> yep
+// /signin --> post = success/fail
+// /register --> post = user
+// /profile/:userid -->get = user
+// /image --> put = user
