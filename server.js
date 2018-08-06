f0ea05926577b8147cf986dfc7ffecc0901032f3
Thomas <tom.n.knol@gmail.com>
2018-08-06 11:19:45 -0500

    adding database support

--- server.js
+++ server.js
@@ -1,91 +1,99 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'someone',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
+
+app.use(cors())
 app.use(bodyParser.json());
 
-const database = {
-  users:[
-    {
-      id: '123',
-      name: 'john',
-      email: 'meatman@mail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'sally',
-      email: 'sizzle@mail.com',
-      password: 'banana',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
-app.get('/', (req, res) => {
+app.get('/', (req, res)=> {
   res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if(user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
-  if(!found) {
-    res.status(404).json('no such user');
-  }
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
 })
 
-app.post('/image', (req, res) => {
+app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if(user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-  if(!found) {
-    res.status(404).json('user not found');
-  }
+  .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.post('/signin', (req, res) => {
-  if(req.body.email == database.users[0].email && req.body.password == database.users[0].password) {
-      res.json('success');
-  } else{
-    res.status(400).json('error logging in')
-  }
-
-})
-
-app.post('/register', (req, res) => {
-  const {email, name, password} = req.body;
-  database.users.push({
-      id: '125',
-      name: name,
-      email: email,
-      password: password,
-      entries: 0,
-      joined: new Date()
-  })
-  res.json(database.users[database.users.length-1]);
-})
-
-app.listen(3000, () => {
+app.listen(3000, ()=> {
   console.log('app is running on port 3000');
-});
-
-// / root -- get --> yep
-// /signin --> post = success/fail
-// /register --> post = user
-// /profile/:userid -->get = user
-// /image --> put = user
+})
